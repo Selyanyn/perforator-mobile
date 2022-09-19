6f1e74ad7127dbf1801e898e1436d13988be13ee
Selyanyn <56536978+Selyanyn@users.noreply.github.com>
2022-09-19 20:59:22 +0500

    SEL-19.09: added most important entities and repositories

---
+++ app/src/main/java/com/example/perforatormobile/domain/entities/Person.kt
@@ -0,0 +1,9 @@
+package com.example.perforatormobile.domain.entities
+
+data class Person(
+    val userId: Int = 0,
+    val profileId: Int = 0,
+    val username: String = "",
+    val photoUrl: String = "",
+    val sbis: String = "",
+)
--- app/src/main/java/com/example/perforatormobile/domain/repository_interfaces/PeersRepository.kt
+++ app/src/main/java/com/example/perforatormobile/domain/repository_interfaces/PeersRepository.kt
@@ -1,4 +1,21 @@
 package com.example.perforatormobile.domain.repository_interfaces
 
+import com.example.perforatormobile.domain.entities.Person
+import kotlinx.coroutines.flow.Flow
+
 interface PeersRepository {
+    fun getAllCurrentUserPeers(): Flow<List<Person>>
+
+    suspend fun deletePeers(ids: List<Int>)
+
+    suspend fun savePeers(ids: List<Int>)
+
+    fun searchPeers(firstName: String): Flow<List<Person>>
+
+    fun getAllTeammates(id: Int): Flow<List<Person>>
+
+    fun getAllSubordinates(id: Int): Flow<List<Person>>
+
+    fun isUserApproved(id: Int): Boolean
+
 }
