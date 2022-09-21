3006d998d988c8f5cb8dfa56c453034101e00acb
Selyanyn <56536978+Selyanyn@users.noreply.github.com>
2022-09-21 17:30:52 +0500

    SEL-21.09: added registration field checks

--- app/src/main/java/com/example/perforatormobile/app/fragments/authorization/RegisterFragment.kt
+++ app/src/main/java/com/example/perforatormobile/app/fragments/authorization/RegisterFragment.kt
@@ -1,4 +1,26 @@
 package com.example.perforatormobile.app.fragments.authorization
 
-class RegisterFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.perforatormobile.databinding.FragmentRegistrationBinding
+import kotlinx.coroutines.flow.combine
+
+class RegisterFragment: Fragment() {
+    private val viewModel: RegisterViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = FragmentRegistrationBinding.inflate(inflater, container, false)
+
+
+
+        return binding.root
+    }
 }
--- app/src/main/java/com/example/perforatormobile/app/fragments/authorization/RegisterViewModel.kt
+++ app/src/main/java/com/example/perforatormobile/app/fragments/authorization/RegisterViewModel.kt
@@ -1,4 +1,66 @@
 package com.example.perforatormobile.app.fragments.authorization
 
-class RegisterViewModel {
+import android.util.Patterns
+import androidx.lifecycle.ViewModel
+import com.example.perforatormobile.R
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class RegisterViewModel: ViewModel() {
+    private val _username = MutableStateFlow("")
+    private val _editUserNameHelper = MutableStateFlow<Int?>(R.string.input_must_be_filled_text)
+    private val _password = MutableStateFlow("")
+    private val _editPasswordHelper = MutableStateFlow<Int?>(R.string.input_must_be_filled_text)
+    private val _phone = MutableStateFlow("")
+    private val _editPhoneHelper = MutableStateFlow<Int?>(R.string.input_must_be_filled_text)
+    private val _sbis = MutableStateFlow("")
+    private val _editSbisHelper = MutableStateFlow<Int?>(R.string.input_must_be_filled_text)
+
+
+    private val _showDialog = MutableStateFlow(false)
+    private val _isUserRegistered = MutableStateFlow(false)
+
+    val showDialog: StateFlow<Boolean> = _showDialog
+    val isUserRegistered: StateFlow<Boolean> = _isUserRegistered
+
+    val editUserNameHelper: StateFlow<Int?> = _editUserNameHelper
+    val editPasswordHelper: StateFlow<Int?> = _editPasswordHelper
+    val editPhoneHelper: StateFlow<Int?> = _editPhoneHelper
+    val editSbisHelper: StateFlow<Int?> = _editSbisHelper
+
+
+    private fun validateForNonEmptyField(field: String, helper: MutableStateFlow<Int?>) {
+        helper.value = if (field.isNotEmpty()) {
+            null
+        } else {
+            R.string.input_must_be_filled_text
+        }
+    }
+
+    private fun validatePassword(password: String) {
+        _editPasswordHelper.value = if (password.isEmpty()) {
+            R.string.input_must_be_filled_text
+        } else if (password.length < 8) {
+            R.string.password_invalid_length
+        } else if (!password.matches(".*[A-ZА-Я].*".toRegex())) {
+            R.string.password_must_contain_uppercase_letters
+        } else if (!password.matches(".*[a-zа-я].*".toRegex())) {
+            R.string.password_must_contain_lowercase_letters
+        } else if (!password.matches(".*[1-9].*".toRegex())) {
+            R.string.password_must_contain_numbers
+        } else
+            null
+    }
+
+    private fun validatePhone(phone: String) {
+        val trimmedPhone = phone.trim()
+        _editPhoneHelper.value = if (trimmedPhone.isEmpty()) {
+            R.string.input_must_be_filled_text
+        } else if (!Patterns.PHONE.matcher(trimmedPhone).matches()) {
+            R.string.incorrect_phone_format
+        } else {
+            null
+        }
+
+    }
 }
