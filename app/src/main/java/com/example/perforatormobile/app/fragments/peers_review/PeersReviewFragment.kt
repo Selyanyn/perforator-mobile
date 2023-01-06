1cd6edc1c90881cb89941829928ce5c56a04c956
Selyanyn <56536978+Selyanyn@users.noreply.github.com>
2023-01-07 01:33:57 +0500

    07.01-SEL: added basic layouts for peers review

---
+++ app/src/main/java/com/example/perforatormobile/app/fragments/peers_review/PeersReviewFragment.kt
@@ -0,0 +1,39 @@
+package com.example.perforatormobile.app.fragments.peers_review
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.example.perforatormobile.R
+import com.example.perforatormobile.app.fragments.verify_chosen_peers.VerifiedPeersListAdapter
+import com.example.perforatormobile.app.fragments.verify_chosen_peers.VerifyChosenPeersFragment
+import com.example.perforatormobile.databinding.FragmentPeersReviewBinding
+
+class PeersReviewFragment: Fragment(R.layout.fragment_peers_review) {
+    private val viewModel: PeersReviewViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = FragmentPeersReviewBinding.inflate(inflater, container, false)
+
+        val reviewedPeersAdapter = VerifiedPeersListAdapter { userId ->
+            val arg = Bundle().apply {
+                putInt(PEER_ID, userId)
+            }
+            findNavController().navigate(R.id.action_navigation_verify_peers_to_chosen_peers, arg)
+        }
+        binding.peersToReviewRecyclerView.adapter = reviewedPeersAdapter
+
+        return binding.root
+    }
+
+    companion object {
+        const val PEER_ID = "peer_id"
+    }
+}
---
+++ app/src/main/java/com/example/perforatormobile/app/fragments/peers_review/PeersReviewViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.perforatormobile.app.fragments.peers_review
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+class PeersReviewViewModel @Inject constructor(): ViewModel() {
+
+}
